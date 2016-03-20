7bde32c4316038877f02dd6e5111043289b45a6e
Efrain <efrabmtz@hotmail.com>
2016-03-19 21:57:32 -0600

    add styles form_for

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,7 +31,7 @@ class PetsController < ApplicationController
 
     respond_to do |format|
       if @pet.save
-        format.html { redirect_to @customer, notice: 'Pet was successfully created.' }
+        format.html { redirect_to @customer, notice: 'Registro de mascota exitoso.' }
         format.json { render :show, status: :created, location: @pet }
       else
         format.html { render :new }
